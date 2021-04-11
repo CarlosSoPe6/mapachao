5f2bcdfdd1ef9eaf9382b9e4e929614b7dbea1ed
Carlos Soto Pérez <carlos348@outlook.com>
2021-04-10 22:30:50 -0500

    File test with docker compose

--- api/server.js
+++ api/server.js
@@ -11,6 +11,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get("/", (req, res) => res.send("Hello raccoons!"));
+app.get("/file_test", (req, res) => res.sendFile(path.join(__dirname, 'media', '123456789.jfif')));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
