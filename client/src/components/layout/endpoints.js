b18203107a2161ea285e3bec9355ee39793f3593
Alejandro Rios <jassoalex97@hotmail.com>
2021-04-18 15:42:52 -0700

    New Endopoints List and ready to add more

---
+++ client/src/components/layout/endpoints.js
@@ -0,0 +1,16 @@
+const ENDPOINT_LIST = [
+  {
+    id: 1,
+    url: "/mapache",
+    description: "Returns a random mapache",
+    example: "Random mapache",
+  },
+  {
+    id: 2,
+    url: "/raccoon",
+    description: "Returns a random raccoon",
+    example: "Random raccoon",
+  },
+];
+
+export default ENDPOINT_LIST;
--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import EndpointsList from "../layout/EndpointsList";
 
 const Home = () => (
   <div className="container mt-5">
@@ -7,11 +8,17 @@ const Home = () => (
       <p className="lead">Mapaches as a service</p>
       <p>Is a REST API where you can get all the cuteness from a raccoon.</p>
       <p className="lead mt-5">
-        <a className="btn btn-primary btn-lg" href="/mapache" target="_blank" role="button">
+        <a
+          className="btn btn-primary btn-lg"
+          href="/mapache"
+          target="_blank"
+          role="button"
+        >
           Get a Mapache
         </a>
       </p>
     </div>
+    <EndpointsList />
   </div>
 );
 
